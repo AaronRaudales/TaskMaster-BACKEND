97fb011b487ffcacf5d75ab90c29bef5157e8937
Aaron Raudales <78047697+AaronRaudales@users.noreply.github.com>
2023-07-25 00:05:30 -0600

    Agregando el Cors

--- index.js
+++ index.js
@@ -15,20 +15,20 @@ dotenv.config();
 conectarDb();
 
 // Poner la url del fronted
-// const dominiosPermitidos = [process.env.FRONTEND_URL];
+const dominiosPermitidos = [process.env.FRONTEND_URL];
 
-// const corsOptions = {
-//     origin: function(origin, callback) { 
-//         if(dominiosPermitidos.indexOf(origin) !== -1) {
-//             //El origen del request esta permitido
-//             callback(null, true)
-//         } else {
-//             callback(new Error('No permitido por CORS'))
-//         }
-//     }
-// }
+const corsOptions = {
+    origin: function(origin, callback) { 
+        if(dominiosPermitidos.indexOf(origin) !== -1) {
+            //El origen del request esta permitido
+            callback(null, true)
+        } else {
+            callback(new Error('No permitido por CORS'))
+        }
+    }
+}
 
-// app.use(cors(corsOptions))
+app.use(cors(corsOptions))
 
 //Direccion del route
 app.use("/api/usuarios", usuarioRoutes);
